f1ece5f03fb93c34729dcb088bde3cb2fef5664d
yeongeun0325 <youngeun@gmail.com>
2016-07-18 17:46:48 +0900

    ....

---
+++ day15/7.js
@@ -0,0 +1,12 @@
+/**
+ * Created by KICT-20 on 2016-07-18.
+ */
+function change(a) {
+    a[0]=7;
+
+}
+//int a[]={3,2,1};
+var a=[3,2,1];
+change(a);
+
+console.log(a)
---
+++ day15/8.js
@@ -0,0 +1,8 @@
+/**
+ * Created by KICT-20 on 2016-07-18.
+ */
+function foo() {
+    console.log("it is foo" )
+}
+var bar=foo;
+bar();
